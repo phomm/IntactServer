54a6c4ab443c9cfcfe4bab248a3c453057296e7c
Gandifil <gandifilmk@gmail.com>
2024-07-07 19:12:10 +0300

    Add filtering for not found ability's index/

--- Intact.BuinessLogic/Mappers/ProtoWarriorMapper.cs
+++ Intact.BuinessLogic/Mappers/ProtoWarriorMapper.cs
@@ -11,8 +11,9 @@ public partial class ProtoWarriorMapper
     public partial ProtoWarrior Map(ProtoWarriorDao dao);
 
     public static IReadOnlyList<ProtoWarrior> Map(IReadOnlyList<ProtoWarriorDao> daos,
-        IReadOnlyList<IGrouping<string, LocalizationDao>> localizations)
+        IReadOnlyList<IGrouping<string, LocalizationDao>> localizations, List<AbilityDao> abilities)
     {
+        var abilityNamesSet = abilities.Select(x => x.Id).ToHashSet();
         var mapper = new ProtoWarriorMapper();
         return daos.Select(d =>
         {
@@ -22,6 +23,9 @@ public partial class ProtoWarriorMapper
             model.Abilities = string.IsNullOrWhiteSpace(d.Abilities)
                 ? null
                 : d.Abilities.Split(',').ToList();
+            var notFoundName = model.Abilities?.FirstOrDefault(x => !abilityNamesSet.Contains(x));
+            if (notFoundName is not null)
+                throw new IndexOutOfRangeException($"The ability '{notFoundName}' is not found");
             return model;
         }).OrderBy(x => x.Number).ToList();
     }
