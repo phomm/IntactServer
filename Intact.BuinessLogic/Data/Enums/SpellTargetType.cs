7ccc2eeabf80474c11daee48dc637c4bfe584207
gandifil <gandifilmk@gmail.com>
2024-10-18 20:52:44 +0300

    Add enums for spells

---
+++ Intact.BuinessLogic/Data/Enums/SpellTargetType.cs
@@ -0,0 +1,12 @@
+﻿namespace Intact.BusinessLogic.Data.Enums;
+
+public enum SpellTargetType
+{
+    None = 0, 
+    OneEnm = 1, 
+    AllEnm = 2, 
+    OneOwn = 3, 
+    AllOwn = 4, 
+    Clear = 5, 
+    Equake = 6
+}
---
+++ Intact.BuinessLogic/Data/Enums/SpellUseType.cs
@@ -0,0 +1,9 @@
+﻿namespace Intact.BusinessLogic.Data.Enums;
+
+public enum SpellUseType
+{
+    None = 0,
+    Free = 1,
+    Limit = 2,
+    Stand = 3,
+}
