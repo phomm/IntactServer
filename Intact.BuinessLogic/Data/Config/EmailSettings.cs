b38e7a74618e235ff810816333039a9ed6a5bfa4
Vlad <phomm@mail.ru>
2025-09-03 15:37:30 +0200

    Add email settings configuration class

---
+++ Intact.BuinessLogic/Data/Config/EmailSettings.cs
@@ -0,0 +1,12 @@
+namespace Intact.BusinessLogic.Data.Config;
+
+public class EmailSettings
+{
+    public string SmtpServer { get; set; } = string.Empty;
+    public int SmtpPort { get; set; }
+    public string SenderEmail { get; set; } = string.Empty;
+    public string SenderName { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public bool EnableSsl { get; set; } = true;
+}
