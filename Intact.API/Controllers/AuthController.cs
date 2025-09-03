389bb12682660a3ac613b1295385317a02b346d7
Vlad <phomm@mail.ru>
2025-09-03 18:22:51 +0200

    Convert comment-only file to proper empty controller structure

--- Intact.API/Controllers/AuthController.cs
+++ Intact.API/Controllers/AuthController.cs
@@ -1,1 +1,10 @@
-// This file has been removed - endpoints moved to IdentityApiAdditionalEndpointsExtensions
+using Microsoft.AspNetCore.Mvc;
+
+namespace Intact.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    // This controller has been removed - endpoints moved to IdentityApiAdditionalEndpointsExtensions
+}
