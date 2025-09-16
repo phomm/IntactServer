152ca1aa8867197e373ba07512335ae8312fa45f
Vlad <phomm@mail.ru>
2025-09-16 19:44:36 +0200

    Edit Intact.API/Controllers/AuthController.cs

--- Intact.API/Controllers/AuthController.cs
+++ Intact.API/Controllers/AuthController.cs
@@ -1,10 +1,1 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Intact.API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class AuthController : ControllerBase
-{
-    // This controller has been removed - endpoints moved to IdentityApiAdditionalEndpointsExtensions
-}
+// AuthController has been removed - all endpoints moved to Identity extensions
