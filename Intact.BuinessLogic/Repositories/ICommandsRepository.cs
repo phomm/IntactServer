1f21605600ba569356787eb57f4afcdc8ca7ba8c
Vlad <phomm@mail.ru>
2026-01-09 12:39:28 +0100

    Add ICommandsRepository interface

--- Intact.BuinessLogic/Repositories/ICommandsRepository.cs
+++ Intact.BuinessLogic/Repositories/ICommandsRepository.cs
@@ -1,21 +1,18 @@
-using Intact.BusinessLogic.Data.Enums;
 using Intact.BusinessLogic.Data.Models;
 
 namespace Intact.BusinessLogic.Repositories;
 
 public interface ICommandsRepository
 {
-    Task<IReadOnlyList<CommandDao>> GetCommandsAsync(int roomId, int offset, CancellationToken cancellationToken);
-    
-    Task<CommandDao?> GetLastCommandAsync(int roomId, CancellationToken cancellationToken);
-    
-    Task<bool> IsPlayerInRoomAsync(int profileId, int roomId, CancellationToken cancellationToken);
-    
-    Task<int> GetRoomPlayerCountAsync(int roomId, CancellationToken cancellationToken);
+    Task<IEnumerable<CommandDao>> GetCommandsAsync(int roomId, int offset, CancellationToken cancellationToken);
     
     Task<uint> GetNextQueueNumberAsync(int roomId, CancellationToken cancellationToken);
     
+    Task<CommandDao?> GetLastCommandAsync(int roomId, CancellationToken cancellationToken);
+    
     Task AddCommandsAsync(IEnumerable<CommandDao> commands, CancellationToken cancellationToken);
     
-    Task<int?> GetPlayerRoomIdAsync(int profileId, CancellationToken cancellationToken);
+    Task<RoomDao?> GetRoomAsync(int roomId, CancellationToken cancellationToken);
+    
+    Task<bool> IsProfileInRoomAsync(int profileId, int roomId, CancellationToken cancellationToken);
 }
