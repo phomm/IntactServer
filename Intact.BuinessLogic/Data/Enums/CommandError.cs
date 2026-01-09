f3648d1e1769a4f44bbd9f196ef9c6a301894a8f
Vlad <phomm@mail.ru>
2026-01-09 12:38:48 +0100

    Add CommandError enum for command validation errors

--- Intact.BuinessLogic/Data/Enums/CommandError.cs
+++ Intact.BuinessLogic/Data/Enums/CommandError.cs
@@ -3,11 +3,9 @@ namespace Intact.BusinessLogic.Data.Enums;
 public enum CommandError : short
 {
     NoError = 0,
-    InvalidPlayer = 1,
+    InvalidCommand = 1,
     NotYourTurn = 2,
-    InvalidCommand = 3,
-    InvalidValue = 4,
-    RoomNotFound = 5,
-    PlayerNotInRoom = 6,
-    GameNotStarted = 7
+    PlayerOutOfRange = 3,
+    InvalidRoomState = 4,
+    InvalidValue = 5
 }
